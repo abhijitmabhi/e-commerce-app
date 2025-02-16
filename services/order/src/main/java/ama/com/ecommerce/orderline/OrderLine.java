c6f3ae4bdc16081403a21f79d309b19dce3a66f8
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2025-02-17 00:47:41 +0100

    (feat): Order entity added

---
+++ services/order/src/main/java/ama/com/ecommerce/orderline/OrderLine.java
@@ -0,0 +1,24 @@
+package ama.com.ecommerce.orderline;
+
+import ama.com.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "order_line")
+public class OrderLine {
+
+  @Id @GeneratedValue private Integer id;
+
+  @ManyToOne
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  private Integer productId;
+  private double quantity;
+}
